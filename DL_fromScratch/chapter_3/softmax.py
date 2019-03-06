203b93a2194e3718618a5a21e5f82d8bbc58b7ac
giy <giy.hands@gmail.com>
2019-03-06 21:51:01 +0900

    MNIST_test

---
+++ DL_fromScratch/chapter_3/softmax.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib as mpl
+mpl.use('TKAgg')
+import matplotlib.pyplot as plt
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
+
