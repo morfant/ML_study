ce715251c1bf30e3a3a6e581703c46017b08dc95
giy <giy.hands@gmail.com>
2019-03-11 19:02:40 +0900

    train

--- DL_fromScratch/chapter_4/mini_batch.py
+++ DL_fromScratch/chapter_4/mini_batch.py
@@ -34,6 +34,33 @@ def numerical_diff(f, x):
     h = 1e-4
     return (f(x + h) - f(x - h)) / (2 * h)
 
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    
+    return x
+
 def function_1(x):
     return 0.01 * x ** 2 + 0.1 * x
     
@@ -54,3 +81,17 @@ d10 = numerical_diff(function_1, 10)
 
 print(d5)
 print(d10)
+
+
+g1 = numerical_gradient(function_2, np.array([3.0, 4.0]))
+g2 = numerical_gradient(function_2, np.array([0.0, 2.0]))
+g3 = numerical_gradient(function_2, np.array([3.0, 0.0]))
+
+print(g1)
+print(g2)
+print(g3)
+
+
+init_x = np.array([-3.0, 4.0])
+gd = gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+print(gd)
