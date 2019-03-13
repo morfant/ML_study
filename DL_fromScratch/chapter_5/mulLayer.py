1059b305fbc731d32a1e21eb0168ece6276efd35
giy <giy.hands@gmail.com>
2019-03-14 00:07:52 +0900

    Add ch5

---
+++ DL_fromScratch/chapter_5/mulLayer.py
@@ -0,0 +1,95 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+
+        return out
+
+    def backword(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+class AddLayer:
+    def __init__(self):
+        pass
+    
+    def forward(self, x, y):
+        out = x + y
+
+        return out
+
+    def backword(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+# apple = 100
+# apple_num = 2
+# tax = 1.1
+
+# mul_apple_layer = MulLayer()
+# mul_tax_layer = MulLayer()
+
+# apple_price = mul_apple_layer.forward(apple, apple_num)
+# price = mul_tax_layer.forward(apple_price, tax)
+
+# print(price)
+
+
+# # -------------------------
+
+
+# dprice = 1
+# dapple_price, dtax = mul_tax_layer.backword(dprice)
+# dapple, dapple_num = mul_apple_layer.backword(dapple_price)
+
+
+
+
+# -------------------------
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+dprice = 1
+dall_price, dtax = mul_tax_layer.backword(dprice)
+dapple_price, dorange_price = add_apple_orange_layer.backword(dall_price)
+dapple, dapple_num = mul_apple_layer.backword(dapple_price)
+dorange, dorange_num = mul_orange_layer.backword(dorange_price)
+
+print(price)
+print(dapple_num, dapple, dorange, dorange_num, dtax)
+
+
+
+
+
+
+
+
+
+
+
+
+
